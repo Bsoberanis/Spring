97cd3389d7d2ac3a56b0c8e62dd297041f5a16aa
Bsoberanis <bsoberanins-2023433@kinal.edu.gt>
2024-07-29 17:04:02 -0600

    primera carga

---
+++ src/main/java/com/brandonsoberanis/primeraapp/PrimeraappApplication.java
@@ -0,0 +1,13 @@
+package com.brandonsoberanis.primeraapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraappApplication.class, args);
+	}
+
+}
